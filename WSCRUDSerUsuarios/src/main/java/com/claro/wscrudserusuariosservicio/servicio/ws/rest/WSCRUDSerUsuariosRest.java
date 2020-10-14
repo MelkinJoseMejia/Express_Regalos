8dbc7d26f9d43722403c8fd86ac31c2acc83b974
user <user@DESKTOP-BNH74I9>
2020-10-14 10:33:51 -0500

    servicio cargueUsuarios

--- WSCRUDSerUsuarios/src/main/java/com/claro/wscrudserusuariosservicio/servicio/ws/rest/WSCRUDSerUsuariosRest.java
+++ WSCRUDSerUsuarios/src/main/java/com/claro/wscrudserusuariosservicio/servicio/ws/rest/WSCRUDSerUsuariosRest.java
@@ -1,10 +1,17 @@
 package com.claro.wscrudserusuariosservicio.servicio.ws.rest;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.util.Iterator;
 import java.util.List;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.apache.poi.ss.usermodel.Row;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -15,14 +22,17 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.claro.wscrudserusuariosservicio.dto.Constantes;
+import com.claro.wscrudserusuariosservicio.dto.SerUsuarios;
 import com.claro.wscrudserusuariosservicio.interfase.SerUsuariosJpaRepository;
+import com.claro.wscrudserusuariosservicio.service.UsuarioService;
 import com.claro.wscrudserusuariosservicio.util.configuracion.Configurador;
 import com.claro.wscrudserusuariosservicio.util.configuracion.ParametrosIniciales;
 import com.claro.wscrudserusuariosservicio.util.configuracion.Propiedades;
 import com.claro.wscrudserusuariosservicio.util.configuracion.UtilsConstantes;
-import com.claro.wscrudserusuariosservicio.dto.SerUsuarios;
+import com.claro.wscrudserusuariosservicio.utils.ExcelHelper;
 
 /**
  * <b>Nombre: </b> WSAppRest </br>
@@ -35,11 +45,11 @@ import com.claro.wscrudserusuariosservicio.dto.SerUsuarios;
  */
 
 @RestController
-@RequestMapping(path = "/mensajes")
+@RequestMapping(path = "/usuario")
 public class WSCRUDSerUsuariosRest {
 
-	@Autowired
-	private SerUsuariosJpaRepository serUsuariosJpaRepository;
+	
+	
 	
 	ParametrosIniciales ini;
 	
@@ -50,7 +60,16 @@ public class WSCRUDSerUsuariosRest {
 
 	private static Logger logger = LogManager.getLogger(UtilsConstantes.LOGGER_PRINCIPAL);
 	private static Propiedades prop = Propiedades.getInstance();
-
+	
+	private final UsuarioService usuarioService;
+	
+	@Autowired
+	private SerUsuariosJpaRepository serUsuariosJpaRepository;
+	
+	
+	public WSCRUDSerUsuariosRest(UsuarioService usuarioService) {
+		this.usuarioService = usuarioService;
+	}
 	
 	@GetMapping(path ="/consultar" , produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity <List<SerUsuarios>> consultar(@RequestParam(name = "UUID", required = false) Long UUID){
@@ -103,6 +122,48 @@ public class WSCRUDSerUsuariosRest {
 			Configurador.cerrarTransaccion(ini, logger);
 		}
 	}
+	
+	
+	@PostMapping("cargarUsuarios")
+	public ResponseEntity<Resource> cargarUsuarios(@RequestParam("file") MultipartFile file) {
+		logger.info("REST request para cargarUsuarios");
+
+		if (!ExcelHelper.hasExcelFormat(file)) {
+			throw new RuntimeException("No se pudo realizar el cargue del archivo excel: Please upload an excel file!\"");
+//			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Please upload an excel file!");
+		}
+		InputStreamResource fileDownload = null;
+		try {
+			ExcelHelper excelHelper = new ExcelHelper();
+			excelHelper.loadFileExcel(file.getInputStream(), Constantes.NOMBRE_HOJA_EXCEL_CARGUE_USUARIOS, 1);
+			Iterator<Row> rows = excelHelper.getRows();
+			while (rows.hasNext()) {
+				Row currentRow = rows.next();
+				String infoUsuario = excelHelper.obtenerfilaArchivo(currentRow, null);
+				if(infoUsuario.equals("")) {
+					break;
+				}
+				String result = usuarioService.cargarUsuario(infoUsuario);
+		        String[] rtaInsercion = result.split("--", 2); 
+		        logger.info(">>>>>>>>>>>>>>>>>>>>>>>>"+infoUsuario);
+		        logger.info(">>>>>>>>>>>>>>>>>>>>>>>>"+result);
+		        
+		        excelHelper.escribirCelda(currentRow, 14, result);
+		        ByteArrayInputStream in = excelHelper.getFile();
+		        fileDownload = new InputStreamResource(in);
+		        
+		        
+			}
+			excelHelper.closeFileExcel();
+			
+		} catch (IOException e) {
+			throw new RuntimeException("No se pudo realizar el cargue del archivo excel: " + e.getMessage());
+		}
+
+		return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename="+Constantes.NOMBRE_ARCHIVO_DESCARGA_CARGUE_USUARIOS )
+                .contentType(MediaType.parseMediaType("application/vnd.ms-excel")).body(fileDownload);
+	}
+	
 	
 	@GetMapping(path ="/resetPropiedades" , produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<String> resetPropiedades() {
